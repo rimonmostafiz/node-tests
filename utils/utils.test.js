fa00a702f88fbf092dbe23f184af089716cade8a
Rimon Mostafiz <ferary.rimon@gmail.com>
2018-07-18 19:20:26 +0600

    Add some more test cases to explore different 'expect' library functionality

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -10,3 +10,38 @@ it('Should square a number', () => {
     var res = utils.square(4);
     expect(res).toBe(16).toBeA('number');
 });
+
+it('Should expect some values', () => {
+    expect(12).toNotBe(11);
+    expect({name: 'Rimon'}).toEqual({name: 'Rimon'});
+    expect([2,3,4]).toInclude(2);
+    expect([2,3,4]).toExclude(1);
+
+    expect({
+        name: 'Rimon', age: 25, location: 'BD'
+    }).toInclude({
+        age: 25
+    });
+
+    expect({
+        name: 'Rimon',
+        age: 25,
+        location: 'BD'
+    }).toExclude({
+        age: 23
+    });
+});
+
+it('Should verify first and last names are set', () => {
+    var user = {age: 25, location: 'BD'};
+    var res = utils.setName(user, 'Rimon Mostafiz');
+
+    expect(user).toEqual(res);
+
+    expect(res)
+        .toInclude({
+            firstName: 'Rimon'
+        }).toInclude({
+            lastName: 'Mostafiz'
+        });
+}) ;
